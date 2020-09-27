91da3c77288935a46eb3432ed6372a7a4bc996b5
Wahyu <wahyuutomo101@live.com>
2020-09-27 22:01:19 +0700

    Change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import AppRoutes from './routes/AppRoutes'
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
