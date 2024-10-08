55309ada838624496af9284e539bcea5f90a0b82
Wahyu <wahyuutomo101@live.com>
2024-10-08 23:09:22 +0700

    modify routing to include sukajadi and lebah tanjung leban pages

--- src/components/Program/Program.js
+++ src/components/Program/Program.js
@@ -1,86 +1,131 @@
-import React from 'react';
-import './Program.css';
+import React from "react";
+import "./Program.css";
 
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import img2 from "../../assets/2.jpeg"
-import img6 from "../../assets/6.jpeg"
-import kda3 from "../../assets/kda3.jpeg"
-import img9 from "../../assets/9.jpeg"
-import poligowo3 from "../../assets/poligowo3.jpeg"
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import img2 from "../../assets/2.jpeg";
+import img6 from "../../assets/6.jpeg";
+import kda3 from "../../assets/kda3.jpeg";
+import img9 from "../../assets/9.jpeg";
+import poligowo3 from "../../assets/poligowo3.jpeg";
+import poligowo2 from "../../assets/poligowo2.jpeg";
+import poligowo1 from "../../assets/20.jpeg";
 
-class Program extends React.Component{
-	render(){
-    const { home } = this.props
-		return(
-		<Row>
-      { home ? null :  
-        (
-          <Col style={{background: '#fff'}}>
-            <Card style={{borderWidth:0}} as="Button" onClick={()=>{this.props.history.push("/home")}}>
-              <Card.Img variant="top" 
-                src={img6}
-                height='240'
-                />
+class Program extends React.Component {
+  render() {
+    const { home } = this.props;
+    return (
+      <Row>
+        {home ? null : (
+          <Col style={{ background: "#fff" }}>
+            <Card
+              style={{ borderWidth: 0 }}
+              as="Button"
+              onClick={() => {
+                this.props.history.push("/home");
+              }}
+            >
+              <Card.Img variant="top" src={img6} height="240" />
               <Card.Body className="text-center">
                 <Card.Title className="text-center">Home</Card.Title>
               </Card.Body>
-              
             </Card>
           </Col>
-        )
-      }
-      
-      <Col style={{background: '#fff'}}>
-        <Card style={{borderWidth:0}} as="Button" onClick={()=>{this.props.history.push("/arboretum")}}>
-          <Card.Img variant="top" 
-            src={img9}
-            height='240'
-            />
-          <Card.Body className="text-center">
-            <Card.Title className="text-center">Arboretum Gambut</Card.Title>
-          </Card.Body>
-          
-        </Card>
-      </Col>
-      <Col style={{background: '#fff'}}>
-        <Card style={{borderWidth:0}} as="Button" onClick={()=>{this.props.history.push("/mangrove-pangkalan-jambi")}}>
-          <Card.Img variant="top" 
-            src={img2}
-            height='240'
-            />
-          <Card.Body className="text-center">
-            <Card.Title className="text-center">Mangrove Pangkalan Jambi</Card.Title>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col style={{background: '#fff'}}>
-        <Card style={{borderWidth:0}} as="Button" onClick={()=>{this.props.history.push("/poligowo")}}>
-          <Card.Img variant="top" 
-            src={poligowo3}
-            height='240'
-            />
-          <Card.Body className="text-center">
-            <Card.Title className="text-center">Penanaman Tanaman Kayu Khas Gambut Metode Poligowo</Card.Title>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col style={{background: '#fff'}}>
-        <Card style={{borderWidth:0}} as="Button" onClick={()=>{this.props.history.push("/konservasi-daerah-aliran")}}>
-          <Card.Img variant="top" 
-            src={kda3}
-            height='240'
-            />
-          <Card.Body className="text-center">
-            <Card.Title>Konservasi Daerah Aliran Sungai Dayang</Card.Title>
-          </Card.Body>
-          
-        </Card>
-      </Col>
-   </Row>
-		);
-	}
+        )}
+
+        <Col style={{ background: "#fff" }}>
+          <Card
+            style={{ borderWidth: 0 }}
+            as="Button"
+            onClick={() => {
+              this.props.history.push("/arboretum");
+            }}
+          >
+            <Card.Img variant="top" src={img9} height="240" />
+            <Card.Body className="text-center">
+              <Card.Title className="text-center">Arboretum Gambut</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col style={{ background: "#fff" }}>
+          <Card
+            style={{ borderWidth: 0 }}
+            as="Button"
+            onClick={() => {
+              this.props.history.push("/mangrove-pangkalan-jambi");
+            }}
+          >
+            <Card.Img variant="top" src={img2} height="240" />
+            <Card.Body className="text-center">
+              <Card.Title className="text-center">
+                Mangrove Pangkalan Jambi
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col style={{ background: "#fff" }}>
+          <Card
+            style={{ borderWidth: 0 }}
+            as="Button"
+            onClick={() => {
+              this.props.history.push("/poligowo");
+            }}
+          >
+            <Card.Img variant="top" src={poligowo3} height="240" />
+            <Card.Body className="text-center">
+              <Card.Title className="text-center">
+                Penanaman Tanaman Kayu Khas Gambut Metode Poligowo
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col style={{ background: "#fff" }}>
+          <Card
+            style={{ borderWidth: 0 }}
+            as="Button"
+            onClick={() => {
+              this.props.history.push("/konservasi-daerah-aliran");
+            }}
+          >
+            <Card.Img variant="top" src={kda3} height="240" />
+            <Card.Body className="text-center">
+              <Card.Title>Konservasi Daerah Aliran Sungai Dayang</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col style={{ background: "#fff" }}>
+          <Card
+            style={{ borderWidth: 0 }}
+            as="Button"
+            onClick={() => {
+              this.props.history.push("/sukajadi");
+            }}
+          >
+            <Card.Img variant="top" src={poligowo2} height="240" />
+            <Card.Body className="text-center">
+              <Card.Title>Arboretum Gambut Sukajadi</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col style={{ background: "#fff" }}>
+          <Card
+            style={{ borderWidth: 0 }}
+            as="Button"
+            onClick={() => {
+              this.props.history.push("/lebah-tanjung-lebah");
+            }}
+          >
+            <Card.Img variant="top" src={poligowo1} height="240" />
+            <Card.Body className="text-center">
+              <Card.Title>Edu Wisata Lebah Madu Tanjung Leban</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default Program;
+
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,23 +1,28 @@
-import * as React from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom';
+import * as React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-import Dashboard from '../views/Dashboard/Dashboard';
-import Arboretum from '../views/Arboretum/Arboretum';
-import MangroveJambi from '../views/MangroveJambi/MangroveJambi';
-import Poligowo from '../views/Poligowo/Poligowo';
-import DaerahAliran from '../views/DaerahAliran/DaerahAliran';
+import Dashboard from "../views/Dashboard/Dashboard";
+import Arboretum from "../views/Arboretum/Arboretum";
+import MangroveJambi from "../views/MangroveJambi/MangroveJambi";
+import Poligowo from "../views/Poligowo/Poligowo";
+import DaerahAliran from "../views/DaerahAliran/DaerahAliran";
+import Sukajadi from "../views/Sukajadi/Sukajadi";
+import LebahTanjungLeban from "../views/LebahTanjungLeban/LebahTanjungLeban";
 
 const AppRoutes = () => {
-    return (
-        <Switch>
-        	<Route path="/home" component={Dashboard} />
-        	<Route path="/arboretum" component={Arboretum} />
-        	<Route path="/mangrove-pangkalan-jambi" component={MangroveJambi} />
-        	<Route path="/poligowo" component={Poligowo} />
-            <Route path="/konservasi-daerah-aliran" component={DaerahAliran} />
-            <Redirect exact from="/" to="home" />
-        </Switch>
-    );
-}
+  return (
+    <Switch>
+      <Route path="/home" component={Dashboard} />
+      <Route path="/arboretum" component={Arboretum} />
+      <Route path="/mangrove-pangkalan-jambi" component={MangroveJambi} />
+      <Route path="/poligowo" component={Poligowo} />
+      <Route path="/konservasi-daerah-aliran" component={DaerahAliran} />
+      <Route path="/sukajadi" component={Sukajadi} />
+      <Route path="/lebah-tanjung-leban" component={LebahTanjungLeban} />
+      <Redirect exact from="/" to="home" />
+    </Switch>
+  );
+};
 
 export default AppRoutes;
+
