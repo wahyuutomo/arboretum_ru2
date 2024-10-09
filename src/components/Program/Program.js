b3c07d3016862cafa04d598d15dcba8f1201806d
Wahyu <wahyuutomo101@live.com>
2024-10-09 18:39:31 +0700

    make program visualization into two rows

--- src/components/Program/Program.js
+++ src/components/Program/Program.js
@@ -9,119 +9,125 @@ import img6 from "../../assets/6.jpeg";
 import kda3 from "../../assets/kda3.jpeg";
 import img9 from "../../assets/9.jpeg";
 import poligowo3 from "../../assets/poligowo3.jpeg";
-import poligowo2 from "../../assets/poligowo2.jpeg";
-import poligowo1 from "../../assets/20.jpeg";
+import tanjungleban from "../../assets/tanjungleban.jpeg";
+import sukajadi from "../../assets/sukajadi.jpg";
 
 class Program extends React.Component {
   render() {
     const { home } = this.props;
     return (
       <Row>
-        {home ? null : (
+        <Row>
+          {home ? null : (
+            <Col style={{ background: "#fff" }}>
+              <Card
+                style={{ borderWidth: 0 }}
+                as="Button"
+                onClick={() => {
+                  this.props.history.push("/home");
+                }}
+              >
+                <Card.Img variant="top" src={img6} height="240" />
+                <Card.Body className="text-center">
+                  <Card.Title className="text-center">Home</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          )}
+
           <Col style={{ background: "#fff" }}>
             <Card
               style={{ borderWidth: 0 }}
               as="Button"
               onClick={() => {
-                this.props.history.push("/home");
+                this.props.history.push("/arboretum");
               }}
             >
-              <Card.Img variant="top" src={img6} height="240" />
+              <Card.Img variant="top" src={img9} height="240" />
               <Card.Body className="text-center">
-                <Card.Title className="text-center">Home</Card.Title>
+                <Card.Title className="text-center">
+                  Arboretum Gambut
+                </Card.Title>
               </Card.Body>
             </Card>
           </Col>
-        )}
-
-        <Col style={{ background: "#fff" }}>
-          <Card
-            style={{ borderWidth: 0 }}
-            as="Button"
-            onClick={() => {
-              this.props.history.push("/arboretum");
-            }}
-          >
-            <Card.Img variant="top" src={img9} height="240" />
-            <Card.Body className="text-center">
-              <Card.Title className="text-center">Arboretum Gambut</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col style={{ background: "#fff" }}>
-          <Card
-            style={{ borderWidth: 0 }}
-            as="Button"
-            onClick={() => {
-              this.props.history.push("/mangrove-pangkalan-jambi");
-            }}
-          >
-            <Card.Img variant="top" src={img2} height="240" />
-            <Card.Body className="text-center">
-              <Card.Title className="text-center">
-                Mangrove Pangkalan Jambi
-              </Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col style={{ background: "#fff" }}>
-          <Card
-            style={{ borderWidth: 0 }}
-            as="Button"
-            onClick={() => {
-              this.props.history.push("/poligowo");
-            }}
-          >
-            <Card.Img variant="top" src={poligowo3} height="240" />
-            <Card.Body className="text-center">
-              <Card.Title className="text-center">
-                Penanaman Tanaman Kayu Khas Gambut Metode Poligowo
-              </Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col style={{ background: "#fff" }}>
-          <Card
-            style={{ borderWidth: 0 }}
-            as="Button"
-            onClick={() => {
-              this.props.history.push("/konservasi-daerah-aliran");
-            }}
-          >
-            <Card.Img variant="top" src={kda3} height="240" />
-            <Card.Body className="text-center">
-              <Card.Title>Konservasi Daerah Aliran Sungai Dayang</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col style={{ background: "#fff" }}>
-          <Card
-            style={{ borderWidth: 0 }}
-            as="Button"
-            onClick={() => {
-              this.props.history.push("/sukajadi");
-            }}
-          >
-            <Card.Img variant="top" src={poligowo2} height="240" />
-            <Card.Body className="text-center">
-              <Card.Title>Arboretum Gambut Sukajadi</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col style={{ background: "#fff" }}>
-          <Card
-            style={{ borderWidth: 0 }}
-            as="Button"
-            onClick={() => {
-              this.props.history.push("/lebah-tanjung-leban");
-            }}
-          >
-            <Card.Img variant="top" src={poligowo1} height="240" />
-            <Card.Body className="text-center">
-              <Card.Title>Edu Wisata Lebah Madu Tanjung Leban</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
+          <Col style={{ background: "#fff" }}>
+            <Card
+              style={{ borderWidth: 0 }}
+              as="Button"
+              onClick={() => {
+                this.props.history.push("/mangrove-pangkalan-jambi");
+              }}
+            >
+              <Card.Img variant="top" src={img2} height="240" />
+              <Card.Body className="text-center">
+                <Card.Title className="text-center">
+                  Mangrove Pangkalan Jambi
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col style={{ background: "#fff" }}>
+            <Card
+              style={{ borderWidth: 0 }}
+              as="Button"
+              onClick={() => {
+                this.props.history.push("/poligowo");
+              }}
+            >
+              <Card.Img variant="top" src={poligowo3} height="240" />
+              <Card.Body className="text-center">
+                <Card.Title className="text-center">
+                  Penanaman Tanaman Kayu Khas Gambut Metode Poligowo
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col style={{ background: "#fff" }}>
+            <Card
+              style={{ borderWidth: 0 }}
+              as="Button"
+              onClick={() => {
+                this.props.history.push("/konservasi-daerah-aliran");
+              }}
+            >
+              <Card.Img variant="top" src={kda3} height="240" />
+              <Card.Body className="text-center">
+                <Card.Title>Konservasi Daerah Aliran Sungai Dayang</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col style={{ background: "#fff" }}>
+            <Card
+              style={{ borderWidth: 0 }}
+              as="Button"
+              onClick={() => {
+                this.props.history.push("/sukajadi");
+              }}
+            >
+              <Card.Img variant="top" src={sukajadi} height="240" />
+              <Card.Body className="text-center">
+                <Card.Title>Arboretum Gambut Sukajadi</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col style={{ background: "#fff" }}>
+            <Card
+              style={{ borderWidth: 0 }}
+              as="Button"
+              onClick={() => {
+                this.props.history.push("/lebah-tanjung-leban");
+              }}
+            >
+              <Card.Img variant="top" src={tanjungleban} height="240" />
+              <Card.Body className="text-center">
+                <Card.Title>Eduwisata Lebah Madu Tanjung Leban</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Row>
     );
   }
